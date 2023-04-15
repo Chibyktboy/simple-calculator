3a0ab8079e1bf2f3f8843dd9bc1a8f61e7b72f9b
Ezechindu Chibuike <cezechindu@gmail.com>
2023-04-15 23:42:09 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [x,setX] = useState("");
+  const [y,setY] = useState("");
+  const [value,setValue] = useState("");
+
+  function add(){
+    const addValue = Number(x + y)
+    setX("");
+    setY("");
+
+   setValue(addValue)
+  }
+  function subtract(){
+    const addValue = (x - y)
+    setX("");
+    setY("");
+   setValue(addValue)
+  }
+  function divide(){
+    const addValue = (x / y)
+    setX("");
+    setY("");
+   setValue(addValue)
+  }
+  function multiply(){
+    const addValue = (x * y);
+    setX("");
+    setY("");
+   setValue(addValue)
+  }
+  
+  function squareRoot(){
+    const addValue = Math.sqrt(x,y);
+    setX("");
+    setY("");
+   setValue(addValue)
+  }
+  function squared(){
+    const addValue = (x * x);
+    setX("");
+    setY("");
+   setValue(addValue)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <input type = "number" placeholder=' x' value={x} onChange={(e) => setX(e.target.value)} />
+      <input type = "number" placeholder='y' value={y} onChange={(e) => setY(e.target.value)} />
     </div>
-  );
+    <button onClick={add}>Add + </button>
+  <button onClick ={subtract}>Sub -</button>
+    <button onClick={multiply}>Mult x</button>
+    <button onClick={divide}>Div /</button>
+    <button onClick={squareRoot}>sqrRt  x</button>
+    <button onClick={squared}>x sqrd</button>
+   
+    <div>
+      <h3>Your Answer Is : {value}</h3>
+    </div>
+    </>
+  )
 }
 
-export default App;
+export default App
